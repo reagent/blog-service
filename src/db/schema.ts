43c65e800dc3c315fbcda6babd951f276bc28d5c
Patrick Reagan <github@the-reagans.com>
2023-01-31 07:53:21 -0700

    Create service class for creating post resources

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,7 +3,18 @@ import { z } from 'zod';
 
 export const sql = createSqlTag({
   typeAliases: {
+    count: z.object({ count: z.number() }),
+
     // Database resources
+    post: z.object({
+      id: z.string(),
+      title: z.string(),
+      body: z.string(),
+      publishedAt: z.date(),
+      createdAt: z.date(),
+      updatedAt: z.date(),
+    }),
+
     token: z.object({
       value: z.string(),
     }),
