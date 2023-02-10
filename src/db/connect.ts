c4c241d56e5fa7cfc009659d3a1538617ae0dc00
Patrick Reagan <github@the-reagans.com>
2023-02-09 17:45:35 -0700

    Log all db queries in Slonik at `debug` level

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,9 +1,13 @@
 import {
   DatabasePool,
+  Interceptor,
   TypeParser,
   createPool,
   createTypeParserPreset,
 } from 'slonik';
+
+import { Loggable, NullLogger } from '@reagent/logging';
+import { LogSource } from '../types';
 
 const timestampParser = (): TypeParser => {
   return {
@@ -13,7 +17,29 @@ const timestampParser = (): TypeParser => {
   };
 };
 
-export const connect = (uri: string): Promise<DatabasePool> => {
+const queryLogger = (
+  logger: Loggable<LogSource>
+): Interceptor['beforeQueryExecution'] => {
+  return (_ctx, query) => {
+    logger.debug('Query', {
+      source: 'database',
+      sql: query.sql,
+      values: query.values,
+    });
+
+    return Promise.resolve(null);
+  };
+};
+
+export const connect = (
+  databaseUrl: string,
+  options?: { logger: Loggable<LogSource> }
+): Promise<DatabasePool> => {
+  const { logger = new NullLogger() } = options || {};
   const typeParsers = [...createTypeParserPreset(), timestampParser()];
-  return createPool(uri, { typeParsers });
+
+  return createPool(databaseUrl, {
+    typeParsers,
+    interceptors: [{ beforeQueryExecution: queryLogger(logger) }],
+  });
 };
