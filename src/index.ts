4e78607ec2ad64942c9b5c01dbb89f40715c479a
Patrick Reagan <github@the-reagans.com>
2023-02-12 07:21:10 -0700

    Configure application-wide logger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
+import path from 'path';
 import dotenv from 'dotenv';
 
 import { createApp } from './app';
 import { connect } from './db/connect';
+import { createLogger } from '@reagent/logging';
+import { LogSource } from './types';
 
 dotenv.config();
 
-const { DATABASE_URL, PORT = '3000' } = process.env;
+const {
+  DATABASE_URL,
+  LOG_LEVEL,
+  LOG_FORMAT = 'plain',
+  NODE_ENV = 'development',
+  PORT = '3000',
+} = process.env;
+
+const logger = createLogger<LogSource>({
+  format: LOG_FORMAT,
+  level: LOG_LEVEL,
+  filePath: path.resolve(__dirname, '..', 'log', `${NODE_ENV}.log`),
+  stdout: NODE_ENV === 'development',
+});
 
 if (!DATABASE_URL) {
   console.error('No database configuration found, please set `DATABASE_URL`');
@@ -13,8 +29,10 @@ if (!DATABASE_URL) {
 }
 
 (async () => {
-  const pool = await connect(DATABASE_URL);
-  const app = createApp({ pool });
+  const pool = await connect(DATABASE_URL, { logger });
+  const app = createApp({ pool, logger });
 
-  app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+  app.listen(PORT, () =>
+    logger.info('Listening', { source: 'server', port: PORT })
+  );
 })();
